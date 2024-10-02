54e59a729101abf78d107dda1570b861dc3468ce
mahmudjon03 <mahmudjonb515gmail.com>
2024-10-02 15:49:00 +0500

    first version

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+namespace WebSite.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option):base(option)
+        { }
+        DbSet<Product> Products { get; set; }
+    }
+}
